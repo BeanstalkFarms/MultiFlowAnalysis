4166f7c8193aed83db48fa846732bc617ad6208c
Soil King <157099073+soilking@users.noreply.github.com>
2024-06-25 14:58:36 -0700

    add missing subgraph folder

---
+++ subgraph/src/utils/Swap.ts
@@ -0,0 +1,55 @@
+import { BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { loadOrCreatePool } from "./Pool";
+import { Swap } from "../../generated/schema";
+import { Swap as SwapEvent } from "../../generated/uniswap/uniswap"
+import { loadOrCreateToken, toDecimal } from "./Token";
+
+export function handleSwapEntity(poolAddress: string, event: SwapEvent): void {
+  let pool = loadOrCreatePool(poolAddress);
+  let prevEntity: Swap | null = null;
+  if (pool.prevSwap != null) {
+    prevEntity = Swap.load(pool.prevSwap!)!;
+    prevEntity.nextEventBlock = event.block.number;
+    prevEntity.blockDiff = prevEntity.nextEventBlock!.minus(prevEntity.eventBlock);
+    prevEntity.save();
+  }
+
+  let entity = createSwapEntity(event);
+  entity.pool = pool.id;
+  entity.eventBlock = event.block.number;
+
+  if (pool.price) {
+    entity.prevPrice = pool.price;
+    entity.prevReserves = pool.reserves;
+  }
+
+  const reserve0bd = toDecimal(pool.reserves![0], loadOrCreateToken(pool.tokens[0]).decimals.toI32());
+  const reserve1bd = toDecimal(pool.reserves![1], loadOrCreateToken(pool.tokens[1]).decimals.toI32());
+
+  entity.newPrice = [reserve1bd.div(reserve0bd), reserve0bd.div(reserve1bd)];
+  entity.newReserves = pool.reserves;
+
+  // Calculate the percent changes
+  if (pool.price && pool.reserves) {
+    entity.percentPriceChange0 = entity.newPrice![0].minus(entity.prevPrice![0]).div(entity.prevPrice![0]);
+    entity.percentPriceChange1 = entity.newPrice![1].minus(entity.prevPrice![1]).div(entity.prevPrice![1]);
+    entity.percentReserveChange0 = (new BigDecimal(entity.newReserves![0].minus(entity.prevReserves![0]))).div(
+      (new BigDecimal(entity.prevReserves![0])));
+    entity.percentReserveChange1 = (new BigDecimal(entity.newReserves![1].minus(entity.prevReserves![1]))).div(
+      (new BigDecimal(entity.prevReserves![1])));
+  }
+
+  entity.txHash = event.transaction.hash.toHexString();
+  entity.save();
+
+  pool.price = entity.newPrice;
+  pool.prevSwap = entity.id;
+  pool.prevEvent = entity.id;
+  pool.prevEventType = "Swap";
+  pool.save();
+}
+
+function createSwapEntity(event: ethereum.Event): Swap {
+  const id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  return new Swap(id);
+}
---
+++ subgraph/tests/uniswap-utils.ts
@@ -0,0 +1,96 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Swap,
+  Sync,
+  Transfer
+} from "../generated/uniswap/uniswap"
+
+export function createSwapEvent(
+  sender: Address,
+  amount0In: BigInt,
+  amount1In: BigInt,
+  amount0Out: BigInt,
+  amount1Out: BigInt,
+  to: Address
+): Swap {
+  let swapEvent = changetype<Swap>(newMockEvent())
+
+  swapEvent.parameters = new Array()
+
+  swapEvent.parameters.push(
+    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
+  )
+  swapEvent.parameters.push(
+    new ethereum.EventParam(
+      "amount0In",
+      ethereum.Value.fromUnsignedBigInt(amount0In)
+    )
+  )
+  swapEvent.parameters.push(
+    new ethereum.EventParam(
+      "amount1In",
+      ethereum.Value.fromUnsignedBigInt(amount1In)
+    )
+  )
+  swapEvent.parameters.push(
+    new ethereum.EventParam(
+      "amount0Out",
+      ethereum.Value.fromUnsignedBigInt(amount0Out)
+    )
+  )
+  swapEvent.parameters.push(
+    new ethereum.EventParam(
+      "amount1Out",
+      ethereum.Value.fromUnsignedBigInt(amount1Out)
+    )
+  )
+  swapEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+
+  return swapEvent
+}
+
+export function createSyncEvent(reserve0: BigInt, reserve1: BigInt): Sync {
+  let syncEvent = changetype<Sync>(newMockEvent())
+
+  syncEvent.parameters = new Array()
+
+  syncEvent.parameters.push(
+    new ethereum.EventParam(
+      "reserve0",
+      ethereum.Value.fromUnsignedBigInt(reserve0)
+    )
+  )
+  syncEvent.parameters.push(
+    new ethereum.EventParam(
+      "reserve1",
+      ethereum.Value.fromUnsignedBigInt(reserve1)
+    )
+  )
+
+  return syncEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  value: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferEvent
+}
